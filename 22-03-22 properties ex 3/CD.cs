54557ebc7dc5b799cbc2ee4c37812f8cf6232aa9
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-03-22 09:30:55 +0100

    Add project files.

---
+++ 22-03-22 properties ex 3/CD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _22_03_22_properties_ex_3
+{
+    class CD
+    {
+        private string titel;
+        private string uitvoerder;
+
+        public string Titel
+        {
+            get { return titel; }
+            set { titel = value; }
+        }
+        public string Uitvoerder
+        {
+            get { return uitvoerder; }
+            set { uitvoerder = value; }
+        }
+        public CD()
+        {
+            titel = "Titel";
+            uitvoerder = "Uitvoerder";
+        }
+
+    }
+}
---
+++ 22-03-22 properties ex 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _22_03_22_properties_ex_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CD CEEDEE = new CD();
+            CEEDEE.Titel = "Muziek";
+            CEEDEE.Uitvoerder = "Jasper Meersschaut";
+
+            Console.WriteLine(CEEDEE.Titel);
+            Console.WriteLine(CEEDEE.Uitvoerder);
+
+        }
+    }
+}
